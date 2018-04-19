64ee4319167617baccde36d697fb4b7bf7ca79bc
Erkin B. Altunbaş <erkinbatu.altunbas@sekomyazilim.com.tr>
2018-04-19 10:13:11 +0300

    Clean up payload

--- src/λℵ.scm
+++ src/λℵ.scm
@@ -5,11 +5,14 @@
   (client-software
    '(((append-string "Discord-Bot (" url ", " version ")"))) version #f))
 
+(define payload (json->string '((username . "bot test")
+                                (content . "hello world"))))
+
 ;; webhook-uri is the URI string containing id and token
 ;; defined in the webhook-secrets module
 (with-input-from-request
  (make-request method: 'POST
                uri: webhook-uri
                headers: (headers '((content-type application/json))))
- (json->string '((content . "hello world")))
+ payload
  print)
