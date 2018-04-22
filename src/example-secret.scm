0ab9c8f79dd4a2ef808beb37e4411e46bc070ab7
erkin <erkinbatu@gmail.com>
2018-04-23 00:09:03 +0300

    Finalise the webhook portion of the bot

--- src/example-secret.scm
+++ src/example-secret.scm
@@ -1,11 +1,7 @@
 ;; Put your webhook secrets here and rename it secret.scm
 ;; Or rename it whatever you want and call it in λℵ.system
-(module webhook-secrets (webhook-uri)
+(module webhook-secrets (webhook-id webhook-token)
   (import chicken scheme)
   (use uri-common)
   (define webhook-id "<your-webhook-id-here>")
-  (define webhook-token "<your-webhook-token-here>")
-  (define webhook-uri
-    (uri-reference
-     (string-append "https://canary.discordapp.com/api/webhooks/"
-                    webhook-id "/" webhook-token))))
+  (define webhook-token "<your-webhook-token-here>"))
