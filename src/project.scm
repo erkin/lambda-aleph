e4a027c1c300ac49976114674ad003e059f49f00
erkin <erkinbatu@gmail.com>
2018-04-26 13:24:45 +0300

    We've got TLS now

--- src/project.scm
+++ src/project.scm
@@ -1,5 +1,5 @@
 (module project *
   (import scheme)
-  (define project-url "http://erkin.co/lambda-aleph")
+  (define project-url "https://erkin.co/lambda-aleph")
   (define project-version "0.0.7")
   (define project-name "λℵ"))
