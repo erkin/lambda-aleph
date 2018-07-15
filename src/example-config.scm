0a327d09f6e407f627774091b7803a7ff41e89b2
erkin <erkinbatu@gmail.com>
2018-07-15 03:31:06 +0300

    Whoops didn't mean to delete this one

--- src/example-config.scm
+++ src/example-config.scm
@@ -3,7 +3,10 @@
 
   (define project-url "https://erkin.co/lambda-aleph")
   (define project-version "0.0.10")
-  (define project-name "λℵ"))
+  (define project-name "λℵ")
+  
+  ;; https://discordapp.com/developers/docs/reference
+  (define api-uri "https://canary.discordapp.com/api"))
 
 (module λℵ-secret *
   (import scheme)
